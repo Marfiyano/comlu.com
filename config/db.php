72ef58ebc4c06feb8850ab145fd7526e5f567030
Gamaliel Marfiyano <marfiyano@hotmail.com>
2016-06-07 23:45:06 +0700

    Ubah Menu Tabular menjadi Menu Order

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=marfiyano',
+    'dsn' => 'mysql:host=localhost;dbname=uas',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -39,7 +39,7 @@ AppAsset::register($this);
             ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
-			['label' => 'Tabular', 'url' => ['/tabular']],
+			['label' => 'Jadwal', 'url' => ['/jadwal']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
             ) : (
