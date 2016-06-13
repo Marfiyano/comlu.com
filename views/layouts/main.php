03225e785fef1562eb7c278dfeba7f51aaabf44c
bob <bob@>
2016-06-13 08:56:05 +0700

    test
    test

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -75,7 +75,7 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; Test <?= date('Y') ?></p>
 
         <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
